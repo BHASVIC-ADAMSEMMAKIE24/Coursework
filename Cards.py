88690a1a589331eb144bceb79154579ed88bc8e2
BHASVIC-ADAMSEMMAKIE24 <adam.semmakie24@students.bhasvic.ac.uk>
2025-10-08 08:33:26 +0100

    wrd,pdf writeups and stage1 code

---
+++ Cards.py
@@ -0,0 +1,64 @@
+# This improves code reusability as the values inside the array change instead making a whole new array
+# when cards are being delt they will be stored as a queue (FIFO)
+#def bjValues():
+one = 1
+two = 2
+three = 3
+four = 4
+five = 5
+six = 6
+seven = 7
+eight = 8
+nine = 9
+ten = 10
+j = 11
+q = 11
+k = 11
+a = 11
+
+#cards = [[one, two, three, four, five, six, seven, eight, nine, ten, "jack", "queen", "king", "ace"], ["Spades", "Hearts",
+ #                                                                                             "Diamonds", "Clubs"]]
+
+cards =  (["AcS","AcH","AcD","AcC"]
+        , ["01S","01H","01D","01C"]
+        , ["02S","02H","02D","02C"]
+        , ["03S","03H","03D","03C"]
+        , ["04S","04H","04D","04C"]
+        , ["05S","05H","05D","05C"]
+        , ["06S","06H","06D","06C"]
+        , ["07S","07H","07D","07C"]
+        , ["08S","08H","08D","08C"]
+        , ["09S","09H","09D","09C"]
+        , ["10S","10H","10D","10C"]
+        , ["JaS","JaH","JaD","JaC"]
+        , ["QuS","QuH","QuD","QuC"]
+        , ["KiS","KiH","KiD","KiC"]
+        )
+
+ace = 0
+one = 1
+two = 2
+three = 3
+four = 4
+five = 5
+six = 6
+seven = 7
+eight = 8
+nine = 9
+ten = 10
+jack = 11
+queen = 12
+king = 13
+spades = 0
+hearts = 1
+diamond = 2
+clubs = 3
+
+print(card[ace][spades])
+
+
+
+
+
+
+#print(str(cards[0][1]) , str(cards[1][0]))
